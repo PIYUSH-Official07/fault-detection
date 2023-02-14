f9fc6b08dfe7146723a756f1af5fbeb91eecb685
Piyush S <singh.piyush575@gmail.com>
2023-02-14 13:40:59 +0530

    train_test_split module included

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -2,6 +2,7 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
+from sklearn.model_selection import train_test_split
 import os,sys
 from pandas import DataFrame
 from sensor.data_access.sensor_data import SensorData
@@ -34,10 +35,29 @@ class DataInngestion:
 
 
     def split_data_as_train_test(self,dataframe: DataFrame)->None:
-        """
-        Export store dataset will be split into train and test file
-        """
-        pass   
+        try:
+            train_set, test_set = train_test_split(
+                            dataframe,test_size=self.data_ingestion_config.train_test_split_ratio
+                                               )
+            """
+            Feature store dataset will be split into train and test file
+            """
+            logging.info("Performed train test split on the dataframe")
+            logging.info("Exited split_data_as_train_test method of Data_Ingestion class")
+
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+
+            logging.info("Exporting train and test file path")
+
+            train_set.to_csv(self.data_ingestion_config.training_file_path,
+                         index=False, header=True)
+            test_set.to_csv(self.data_ingestion_config.testing_file_path,
+                         index=False, header=True)
+            logging.info("Exported train and test file path")
+        except Exception as e:
+            raise SensorData(e,sys)
+
     
     def initiate_data_ingestion(self)->DataIngestionArtifact:
         try:
